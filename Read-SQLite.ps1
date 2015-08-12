423adb60be48d1679629c0489f507c9c59debbb2
Marco van Gaal <m.vangaal@kobe.eu>
2015-08-12 14:06:30 +0200

    Add ReadObject parameter to output more pipeline aware but with a small speed penalty

--- Read-SQLite.ps1
+++ Read-SQLite.ps1
@@ -7,10 +7,10 @@
            	Reads through the data, writing it to a DataTable Object. Finally closes the SQLiteDataReader Object.
 
 			.INPUTS
-            <String>
+            String
 
-            .OUTPUTS
-           	<DataTable>
+            .PARAMETER ReturnObject
+            this outputs records line by line this way its more pipeline aware but but with a decrease in speed
 			
             .EXAMPLE
             PS C:\> Read-SQLite [-Connection] <SQLiteConnection> [-Query] <String>
@@ -29,16 +29,46 @@
             .LINK
             http://www.proxx.nl/Wiki/Read-SQLite
 #>
+    [OutputType("DataTable", ParameterSetName="Table")]
+    [OutputType("Object[]", ParameterSetName="Object")]
 	param(
-		[Parameter(Mandatory=$True, ValueFromPipeline=$false)][System.Data.SQLite.SQLiteConnection] $Connection
-		,[Parameter(Mandatory=$True, ValueFromPipeline=$True)][String]$Query)
+		[Parameter(ParameterSetName="Table", Mandatory=$True, ValueFromPipeline=$false)]
+        [Parameter(ParameterSetName="Object", Mandatory=$True, ValueFromPipeline=$false)]
+            [System.Data.SQLite.SQLiteConnection] $Connection,
+		[Parameter(ParameterSetName="Table", Mandatory=$True, ValueFromPipeline=$True)]
+        [Parameter(ParameterSetName="Object", Mandatory=$True, ValueFromPipeline=$True)]
+            [String]$Query,
+        [Parameter(ParameterSetName="Object", Mandatory=$True, ValueFromPipeline=$True)]
+            [Switch] $ReturnObject
+    )
 	
 	if ($Connection.State -ne "Open") { Throw "Connection has state: " + $Connection.State + ". the connection must be Open in order to proceed" }
 	#$datatSet = New-Object System.Data.DataSet
-	$dataAdapter = New-Object -TypeName System.Data.SQLite.SQLiteDataAdapter -ArgumentList $Query,$Connection
-	$dataTable = New-Object -TypeName System.Data.DataTable 
-	#[void]$dataAdapter.Fill($datatSet)
-	[void]$dataAdapter.Fill($dataTable) 
+    if ($ReturnObject) 
+    {
+	    $Command = $Connection.CreateCommand()
+        $command.CommandText = $Query
+        $Reader = $command.ExecuteReader()
 
-	return @(,($DataTable))
+        if ($Reader.HasRows) 
+        {
+            $FieldCount = $Reader.FieldCount
+            While($Reader.Read()) {
+                $Object = New-Object PSObject -Property @{}
+                for ([int] $i = 0; $i -lt $FieldCount; $i++) { 
+                    Add-Member -InputObject $Object -Name $Reader.GetName($i) -Value $Reader.GetValue($i) -MemberType NoteProperty -Force
+                }
+                Write-Output -InputObject $Object
+            }
+        }
+    }
+    Else
+    {
+	    $dataAdapter = New-Object -TypeName System.Data.SQLite.SQLiteDataAdapter -ArgumentList $Query,$Connection
+	    $dataTable = New-Object -TypeName System.Data.DataTable 
+	    #[void]$dataAdapter.Fill($datatSet)
+	    [void]$dataAdapter.Fill($dataTable) 
+
+	    return @(,($DataTable))
+    }
 }
