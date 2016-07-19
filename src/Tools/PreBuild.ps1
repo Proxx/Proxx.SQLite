f4de2fe0ae43d1866a9526e6302ddf778d9e837c
Marco van Gaal <marco@proxx.nl>
2016-07-19 11:18:31 +0200

    fixed prebuild script

--- src/Tools/PreBuild.ps1
+++ src/Tools/PreBuild.ps1
@@ -7,7 +7,7 @@ $AssemblyInfo="AssemblyInfo.cs"
 
 
 #Get current build version
-[Version] $CurrentVersion = ([Version] ($([reflection.assembly]::LoadFile($BinaryPath)).ToString() -split ", " | ? { $_ -like "Version*" } | ConvertFrom-StringData)).Version
+[Version] $CurrentVersion = ([Version] ($([reflection.assembly]::LoadFile($BinaryPath)).ToString() -split ", " | ? { $_ -like "Version*" } | ConvertFrom-StringData).Version)
 Write-Host "Current version is $CurrentVersion"
 [Version] $NewVersion = "{0}.{1}.{2}.{3}" -f $CurrentVersion.Major, $CurrentVersion.Minor, $CurrentVersion.Build, $($CurrentVersion.Revision + 1)
 Write-Host "New version is $NewVersion"
