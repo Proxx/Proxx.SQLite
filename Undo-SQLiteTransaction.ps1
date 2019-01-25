4b29ead06e552b7d2d4f5d45fdc58f8e43f2df02
Marco van Gaal <marco@proxx.nl>
2019-01-25 09:30:24 +0100

    Added Undo-SQLiteTransaction to rollback transactions

---
+++ Undo-SQLiteTransaction.ps1
@@ -0,0 +1,24 @@
+﻿Function Undo-SQLiteTransaction {
+<#
+.Synopsis
+   End SQLite Transaction.
+.DESCRIPTION
+   Invoke Rollback on the transaction object.
+.EXAMPLE
+   $Transaction | Undo-SQLiteTransaction -Rollback
+.EXAMPLE
+   Undo-SQLiteTransaction -Transaction $Transaction
+.NOTES
+   using the method 'Rollback()' on the transaction object is shorter
+   this function is to make the module as complete as possible.
+.LINK
+    http://www.proxx.nl/Module/SQLite/
+#>
+    Param(
+        [ALias("trns")]
+        [Parameter(ValueFromPipeline=$true, mandatory=$true)]
+        [System.Data.SQLite.SQLiteTransaction] $Transaction
+    )
+    
+    Return $Transaction.Rollback()
+}
