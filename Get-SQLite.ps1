bce5357c15ac0d9560610556f281c7dd94ad62cd
Marco van Gaal <marco@proxx.nl>
2019-01-25 11:05:37 +0100

    Inverterd the ReturnObject to default instead there is ReturnDataTable

--- Get-SQLite.ps1
+++ Get-SQLite.ps1
@@ -9,7 +9,7 @@
 	.INPUTS
         System.String
 
-    .PARAMETER ReturnObject
+    .PARAMETER ReturnDataTable
         Outputs records line by line this way its more pipeline aware but with a decrease in speed
 			
     .EXAMPLE
@@ -37,51 +37,70 @@
 		[Parameter(ParameterSetName="Table", Mandatory=$True, ValueFromPipeline=$True)]
         [Parameter(ParameterSetName="Object", Mandatory=$True, ValueFromPipeline=$True)]
             [String]$Query,
+        [Parameter(ParameterSetName="Table", Mandatory=$True, ValueFromPipeline=$True)]
+            [Switch] $ReturnDataTable,
+        [Parameter(ParameterSetName="Table", Mandatory=$True, ValueFromPipeline=$True)]
         [Parameter(ParameterSetName="Object", Mandatory=$True, ValueFromPipeline=$True)]
-            [Switch] $ReturnObject,
-            [Hashtable] $Parameters
+            [Hashtable] $Parameters,
+
     )
-	
+	# Throw when there is no connection to database
 	if ($Connection.State -ne "Open") { Throw "Connection has state: " + $Connection.State + ". the connection must be Open in order to proceed" }
-    #$datatSet = New-Object System.Data.DataSet
-    
+
+    # Create SQLite command
     $Command = $Connection.CreateCommand()
+
+    # add Query to command
     $command.CommandText = $Query
-    Write-Host $Query
+
     if ($PSBoundParameters.ContainsKey("Parameters"))
     {
         ForEach($Param in $Parameters.GetEnumerator())
         {
+            # add all defined parameters from hashtable to command.
             [Void] $Command.Parameters.Add([System.Data.SQLite.SQLiteParameter]::new($Param.Key, $Param.Value))
         }
     }
 
-    if ($ReturnObject) 
+    # Return DataTable object instead of PSObject to pipe
+    if ($ReturnDataTable)
     {
+        # create SQLiteDataAdapter object
+        $dataAdapter = New-Object -TypeName System.Data.SQLite.SQLiteDataAdapter
 
+        # add command to adapter
+        $dataAdapter.SelectCommand = $Command
+        
+        # create new empty DataTable object
+	    $dataTable = New-Object -TypeName System.Data.DataTable 
+
+        # fill DataTable with data returned by the adapter.
+	    [void]$dataAdapter.Fill($dataTable) 
+
+        # return
+	    return @(,($DataTable))
+    }
+    Else
+    {
+        # Execute the command
         $Reader = $command.ExecuteReader()
 
+        # check if the command returned any rows.
         if ($Reader.HasRows) 
         {
             $FieldCount = $Reader.FieldCount
             While($Reader.Read()) {
+                # create empty PSObject
                 $Object = New-Object -TypeName PSObject -Property @{}
+
+                # loop trough fields
                 for ([int] $i = 0; $i -lt $FieldCount; $i++) { 
+                    # add members to PSobject
                     Add-Member -InputObject $Object -Name $Reader.GetName($i) -Value $Reader.GetValue($i) -MemberType NoteProperty -Force
                 }
+                # write output to pipe.
                 Write-Output -InputObject $Object
             }
         }
-    }
-    Else
-    {
-        $dataAdapter = New-Object -TypeName System.Data.SQLite.SQLiteDataAdapter
-        $dataAdapter.SelectCommand = $Command
-        
-	    $dataTable = New-Object -TypeName System.Data.DataTable 
-	    #[void]$dataAdapter.Fill($datatSet)
-	    [void]$dataAdapter.Fill($dataTable) 
-
-	    return @(,($DataTable))
     }
 }
