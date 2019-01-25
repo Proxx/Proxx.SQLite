198753211713a073fef6ca831c0534a32b007be3
Marco van Gaal <marco@proxx.nl>
2019-01-25 11:14:24 +0100

    updated module file to load functions from seperate folders

--- Proxx.SQLite.psm1
+++ Proxx.SQLite.psm1
@@ -6,9 +6,18 @@
 #
 # Website: www.proxx.nl
 #
-Resolve-Path -Path $PSScriptRoot\*.ps1 | ForEach-Object { . $_.ProviderPath }
+
+# Load all main functions
+Resolve-Path -Path $PSScriptRoot\main\*.ps1 | ForEach-Object { . $_.ProviderPath }
+
+# Load all extra functions
+Resolve-Path -Path $PSScriptRoot\tools\*.ps1 | ForEach-Object { . $_.ProviderPath }
+
+# Export members
 Export-ModuleMember -Function Compress-SQLite, Connect-SQLite, Convert-SQLiteType, Disconnect-SQLite, Get-SQLiteTables, Invoke-SQLiteFill, Invoke-SQLiteTable, New-SQLiteTable, Out-SQLiteTable, Read-SQLite, New-SQLiteTransaction, Complete-SQLiteTransaction, Undo-SQLiteTransaction, Write-SQLite
 
+
+# Create alias names for functions
 Set-Alias -Name Vacuum -Value Compress-SQLite
 Set-Alias -Name BeginTransaction -Value New-SQLiteTransaction
 Set-Alias -Name RollbackTransaction -Value Undo-SQLiteTransaction
