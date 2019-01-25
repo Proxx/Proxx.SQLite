36ba88c294ec2334a379794fa161b4f36f2565fa
Marco van Gaal <marco@proxx.nl>
2019-01-25 11:33:54 +0100

    changed pester tests for Read-SQLite

--- Tests/Read-SQLite.Tests.ps1
+++ Tests/Read-SQLite.Tests.ps1
@@ -1,4 +1,4 @@
-﻿Describe "Read-SQL" {
+﻿Describe "Get-SQLite" {
     BeforeAll {
         $conn = Connect-SQLite -Memory -Open
         Write-SQLite -Connection $conn -Query "CREATE TABLE a (x TEXT, y, z);" | Out-Null
@@ -11,36 +11,23 @@
         $conn.Dispose()
         Remove-Variable -name conn -Force
     }
-    It "should not throw" {
-        { Read-SQLite -Connection $conn -Query "SELECT x FROM A LIMIT 3;"  } | Should Not Throw
+    It "Should not throw" {
+        { Get-SQLite -Connection $conn -Query "SELECT x FROM A LIMIT 3;"  } | Should Not Throw
     }
-    It "returns datatable type" {
-        (Read-SQLite -Connection $conn -Query "SELECT x FROM A LIMIT 3;") -is [System.Data.DataTable] | Should Be $true
+    It "Returns DataTable type" {
+        (Get-SQLite -Connection $conn -Query "SELECT x FROM A LIMIT 3;" -ReturnDataTable) -is [System.Data.DataTable] | Should Be $true
     }
-    It "datatable should contain 20 rows" {
-        (Read-SQLite -Connection $conn -Query "SELECT x FROM A LIMIT 20;").Rows.Count | Should Be 20
+    It "Returns PSObject type" {
+        (Get-SQLite -Connection $conn -Query "SELECT x FROM A LIMIT 3;") -is [System.Object] | Should Be $true
     }
-    it "should throw when connection is not SQLiteConnection type" {
+    It "PSObject should contain 20 rows" {
+        (Read-SQLite -Connection $conn -Query "SELECT x FROM A LIMIT 20;").Count | Should Be 20
+    }
+    it "Should throw when connection is not SQLiteConnection type" {
         { Read-SQLite -connection "test" -Query "" } | Should Throw
     }
-    Context "ReturnObject" {
-        BeforeAll {
-            $conn = Connect-SQLite -Memory -Open
-            Write-SQLite -Connection $conn -Query "CREATE TABLE a (x TEXT, y, z);"
-            $trans = $conn.BeginTransaction()
-            1..20 | %{ Write-SQLite -Connection $conn -Query "INSERT INTO a ('x','y','z') VALUES ('1','2','3');" | Out-Null }
-            $trans.Commit()
-        }
-        AfterAll {
-            $conn.close()
-            $conn.Dispose()
-            Remove-Variable -name conn -Force
-        }
-        it "returns Object when switch ReturnObject is used" {
-            (Read-SQLite -Connection $conn -Query "SELECT x FROM A LIMIT 3;" -ReturnObject) -is [System.Object] | Should Be $true
-        }
-        it "should read 20 rows from table" {
-            (Read-SQLite -Connection $conn -Query "SELECT x FROM A LIMIT 20;" -ReturnObject).Count | Should Be 20
-        }
+    it "Should throw when connection is not Open" {
+        $conn.Close()
+        { Read-SQLite -connection $conn -Query "SELECT" } | Should Throw
     }
 }
